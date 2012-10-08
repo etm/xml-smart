8ad9a3486593e7fd025b1fbb34b3e6318feaedb6
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2012-10-08 21:29:43 +0200

    tc_write test

---
+++ test/tc_write.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+require File.expand_path(::File.dirname(__FILE__) + '/../lib/xml/smart')
+require File.expand_path(::File.dirname(__FILE__) + '/smartrunner.rb')
+
+class TestBasic < Test::Unit::TestCase
+  def test_criss_cross
+    tim = Time.now
+
+    # When a closure is used, then the changes are written back to the disk
+    XML::Smart.modify(::File.dirname(__FILE__) + "/EXAMPLE.xml") do |doc|
+      node = doc.root.find("/test/names/name")[0]
+      node.text = tim
+    end
+    
+    # Print the document
+    assert(XML::Smart.open(::File.dirname(__FILE__) + "/EXAMPLE.xml").find("string(/test/names/name)") == tim.to_s)
+  end
+end
